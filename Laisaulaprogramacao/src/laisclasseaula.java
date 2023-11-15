ed1025578a9c20b87cd36d38f76f5488d81e33e1
robson.baia <robson.baia@JNV0604866.jv01.local>
2023-11-15 20:58:19 -0300

    add

--- Laisaulaprogramacao/src/laisclasseaula.java
+++ Laisaulaprogramacao/src/laisclasseaula.java
@@ -11,20 +11,7 @@ class laisclasseaula {
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-<<<<<<< master
-	private int datanascimento;
 
-	public int getDatanascimento() {
-		return datanascimento;
-	}
-
-	public void setDatanascimento(int datanascimento) {
-		this.datanascimento = datanascimento;
-	}
-
-=======
->>>>>>> 1fcab55 oi
-	
 	}
 	
 	
