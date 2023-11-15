8dcbb5aa4fc4dfd604744dd78792a0acfe0cad65
robson.baia <robson.baia@JNV0604866.jv01.local>
2023-11-15 20:36:45 -0300

    comitanto projeto

--- Laisaulaprogramacao/src/laisclasseaula.java
+++ Laisaulaprogramacao/src/laisclasseaula.java
@@ -2,6 +2,15 @@
 class laisclasseaula {
 
 	/* Comentarios lais */
+	private int idade;
 
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
 }
 	
