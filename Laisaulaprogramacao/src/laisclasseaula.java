3419bb3fd2afb75592c74da461b89bc6e383856c
robson.baia <robson.baia@JNV0604866.jv01.local>
2023-11-17 15:29:36 -0300

    Revert "add"
    This reverts commit d0f55057ccd54d90d7b7ac80b8491fb07323cbd6.

--- Laisaulaprogramacao/src/laisclasseaula.java
+++ Laisaulaprogramacao/src/laisclasseaula.java
@@ -1,24 +1,8 @@
-import sun.util.calendar.BaseCalendar.Date;
 
-/**
- * @author robson.baia
- *
- */
 class laisclasseaula {
 
 	/* Comentarios lais */
 	private int idade;
-	
-	private Date datanascimento;
-	
-
-	public Date getDatanascimento() {
-		return datanascimento;
-	}
-
-	public void setDatanascimento(Date datanascimento) {
-		this.datanascimento = datanascimento;
-	}
 
 	public int getIdade() {
 		return idade;
@@ -26,7 +10,6 @@ class laisclasseaula {
 
 	public void setIdade(int idade) {
 		this.idade = idade;
-		
 	}
 
 	}
