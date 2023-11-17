8e44a401ac621436523aa133355c86bedf4d6f26
robson.baia <robson.baia@JNV0604866.jv01.local>
2023-11-17 16:32:20 -0300

    Código da tarefa 1 da branch 1

--- Laisaulaprogramacao/src/laisclasseaula.java
+++ Laisaulaprogramacao/src/laisclasseaula.java
@@ -3,6 +3,16 @@ class laisclasseaula {
 
 	/* Comentarios lais */
 	private int idade;
+	
+	private string nome;
+	
+	public string getNome() {
+		return nome;
+	}
+
+	public void setNome(string nome) {
+		this.nome = nome;
+	}
 
 	public int getIdade() {
 		return idade;
